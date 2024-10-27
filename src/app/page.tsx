b80c64e0dc9cbeda93ca01747b7015dd9d8397a0
Doyen Audrey <audrey@metis-lab.com>
2024-10-27 14:03:49 +0100

    Folders structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-import { AdventGrid } from "./components/AdventGrid";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
+import { AdventGrid } from "../components/AdventGrid";
 
 export default function Home() {
   return (
