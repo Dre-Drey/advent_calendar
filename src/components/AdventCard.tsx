371d9e8e5d3f43cf10fb58302998b37a6f518a8b
Doyen Audrey <audrey@metis-lab.com>
2024-11-02 16:06:18 +0100

    Add modal

--- src/components/AdventCard.tsx
+++ src/components/AdventCard.tsx
@@ -1,14 +1,74 @@
 "useClient";
-import { Paper, Title } from "@mantine/core";
+import {
+  Paper,
+  Title,
+  Stack,
+  Button,
+  Modal,
+  Card,
+  Image,
+  Text,
+  Badge,
+  Group,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import classes from "../style/adventCard.module.css";
 
-export function AdventCard({ date, children }) {
-  // const imagePath = "/icons/";
+type AdventCardProps = {
+  date: number;
+  children: React.ReactNode;
+  isCardEnabled: boolean;
+  category: string;
+  image: string;
+  title: string;
+  description: string;
+};
+
+export function AdventCard({
+  date,
+  children,
+  isCardEnabled,
+  category,
+  image,
+  title,
+  description,
+}: AdventCardProps) {
+  const [opened, { close, open }] = useDisclosure(false);
   return (
-    <Paper className={classes.paper} shadow="md">
-      <Title order={4}>{date}</Title>
-      {/* <icon alt="icone de la carte" c="blue.7" /> */}
-      {children}
-    </Paper>
+    <>
+      <Modal opened={opened} onClose={close} size="lg" withCloseButton={false}>
+        <Card>
+          <Card.Section mb="lg">
+            <Badge color="blue.1">{category}</Badge>
+            <Group justify="space-between" my="lg">
+              <Title c="blue.7">{title}</Title>
+            </Group>
+            <Text size="sm">{description}</Text>
+          </Card.Section>
+
+          <Card.Section>
+            <Image src={image} height={160} alt={title} />
+          </Card.Section>
+        </Card>
+      </Modal>
+      <Paper
+        // className={isCardEnabled ? classes.activePaper : classes.disabledPaper}
+        className={classes.activePaper}
+        shadow="md"
+      >
+        <Title order={4}>{date}</Title>
+        <Stack justify="center" align="center">
+          {children}
+          <Button
+            size="compact-md"
+            variant="filled"
+            color="lightBeige.5"
+            onClick={open}
+          >
+            Ouvrir
+          </Button>
+        </Stack>
+      </Paper>
+    </>
   );
 }
