f4ec1cd6c6ad4150b7722a08178025cf9317865b
Doyen Audrey <audrey@metis-lab.com>
2024-11-02 16:06:51 +0100

    style grid

--- src/components/AdventGrid.tsx
+++ src/components/AdventGrid.tsx
@@ -5,12 +5,26 @@ import { data } from "../data/data";
 import { AdventCard } from "./AdventCard";
 
 export function AdventGrid() {
+  const isCardEnabled = (day: number) => {
+    const cardDate = new Date(new Date().getFullYear(), 11, day);
+    const currentDate = new Date();
+    return cardDate <= currentDate;
+  };
+
   return (
     <Container size="md">
       <Group justify="space-around">
         {data.map((item) => (
-          <AdventCard key={item.date} date={item.date}>
-            <item.icon size={60} stroke={1} style={{ margin: "auto" }} />
+          <AdventCard
+            key={item.date}
+            date={item.date}
+            isCardEnabled={isCardEnabled(item.date)}
+            category={item.category}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          >
+            <item.icon size={60} stroke={1} />
           </AdventCard>
         ))}
       </Group>
